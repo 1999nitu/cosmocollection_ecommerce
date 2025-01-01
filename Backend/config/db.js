327b64d9b02cc4949de52702d71fb071012ecf83
1999nitu <157201637+1999nitu@users.noreply.github.com>
2025-01-01 09:43:45 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require ('mongoose')
 
-mongoose.connect("mongodb://localhost:27017/project1")
+mongoose.connect("mongodb+srv://kaurnavprit5:OYBzcIHo1t4uqYLb@cluster0.lssgc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 .then(() =>{
     console.log("dataBase is connected")
 })
